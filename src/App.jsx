00432160ece60108089ee58d52d167c6df57fbe3
Shovan2024 <2022pms5223@mnit.ac.in>
2024-06-23 23:05:39 +0530

    Initial commit of dashboard files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
+// import { ModeToggle } from "./components/mode-toggle";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectView from "./pages/ProjectView";
 import Footer from "./pages/miniComponents/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+
+// Component for handling the /admin redirect
+const AdminRedirect = () => {
+  useEffect(() => {
+    window.location.href = 'https://dashboardfolio-ty57.vercel.app/login';
+  }, []);
+
+  return null;
+};
 
 function App() {
   return (
@@ -15,6 +25,7 @@ function App() {
         {/* <ModeToggle /> */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<AdminRedirect />} />
           <Route path="/project/:id" element={<ProjectView />} />
         </Routes>
         <Footer />
