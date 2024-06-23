1d0bf0fffa5724ceb6c9ba2e91dcc4c0b0286b4f
Shovan2024 <2022pms5223@mnit.ac.in>
2024-06-24 00:40:09 +0530

    Initial commit of dashboard files

--- src/pages/miniComponents/Contact.jsx
+++ src/pages/miniComponents/Contact.jsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 import { toast } from "react-toastify";
 
 const Contact = () => {
