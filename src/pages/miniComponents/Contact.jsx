137ac40211ac213b757b32591e6427081c195d15
shovan.mnit <141280671+Shovan2024@users.noreply.github.com>
2024-10-02 13:17:53 +0530

    Update Contact.jsx

--- src/pages/miniComponents/Contact.jsx
+++ src/pages/miniComponents/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     setLoading(true);
     await axios
       .post(
-        "https://websitebackend-production-97ac.up.railway.app/api/v1/message/send",
+        "https://websitebackend-production-7b1e.up.railway.app/api/v1/message/send",
         { senderName, subject, message },
         {
           withCredentials: true,
