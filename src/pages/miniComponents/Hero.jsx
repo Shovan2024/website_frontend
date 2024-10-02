3013637cfa4a5a9d307d49f476ec22519c565c94
shovan.mnit <141280671+Shovan2024@users.noreply.github.com>
2024-10-02 13:17:13 +0530

    Update Hero.jsx

--- src/pages/miniComponents/Hero.jsx
+++ src/pages/miniComponents/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
   useEffect(() => {
     const getMyProfile = async () => {
       const { data } = await axios.get(
-        "https://website-backend-coral.vercel.app/api/v1/user/portfolio/me",
+        "https://websitebackend-production-7b1e.up.railway.app/api/v1/user/portfolio/me",
         { withCredentials: true }
       );
       setUser(data.user);
